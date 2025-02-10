be656124b70bed034bd87e55505b3766d8f1a52e
ermiich <ermichyt@gmail.com>
2025-02-10 20:15:51 +0000

    preeentrega

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,203 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, onMounted, ref } from 'vue';
+import InsertName from './components/InsertName.vue'
+import codes from "./data/codes.json";
+import GoogleChart from './components/GoogleChart.vue';
+
+const selectedCodes = ref([]);
+const selectedCountry = ref('');
+const currentCountryData = ref(null);
+const countriesNamesWithCodes = ref([]);
+
+const dataTypes = ['population', 'pib', 'area', 'income'];
+const selectedDataType = ref('population');
+
+const dataToGchart = ref([null, null]);
+
+
+async function fetchInfoByCode(code) {
+  const response = await fetch('http://localhost:3000/api/country/' + code);
+  const data = await response.json();
+  return data ? data : null;
+}
+
+function dataToChart() {
+  let aux = [];
+  selectedCodes.value.forEach(code => {
+    const name = countriesNamesWithCodes.value[code];
+    const data = currentCountryData.value[selectedDataType.value];
+    aux.push([name, data]);
+  });
+
+  console.log(aux);
+}
+
+const newSelectedHandler = async (code) => {
+  if (!selectedCodes.value.includes(code)) {
+    const data = await fetchInfoByCode(code);
+    if (data) {
+      selectedCountry.value = code;
+      selectedCodes.value.push(code);
+      console.log(data);
+      currentCountryData.value = data;
+      return;
+    }
+    console.log('Pais no encontrado ' + code);
+
+  }
+};
+const removeSelected = (code) => {
+  selectedCodes.value = selectedCodes.value.filter((c) => c !== code);
+};
+onMounted(async () => {
+  const response = await fetch('http://localhost:3000/api/names');
+  const data = await response.json();
+  console.log(data);
+  countriesNamesWithCodes.value = data;
+  // Array.sort(countriesNamesWithCodes.value, (a, b) => a.localeCompare(b));
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="parent">
+    <div class="header">
+      <h1>Datos mundiales</h1>
+    </div>
+    <div class="name">
+      <InsertName />
+    </div>
+    <div class="div-codes">
+      <ul>
+        <li v-for="(code, index) in codes" :key="index" @click="newSelectedHandler(code)">{{
+          countriesNamesWithCodes[code] }}</li>
+      </ul>
+    </div>
+    <div class="selected">
+
+      <p v-if="selectedCodes.length > 0">
+      <h2>Códigos seleccionados {{ selectedCodes.length }}</h2>
+      <ul>
+        <li v-for="(code, index) in selectedCodes" :key="index">{{ countriesNamesWithCodes[code] }}
+          <button @click="removeSelected(code)">Desmarcar</button>
+        </li>
+      </ul>
+      </p>
+
+      <p v-else>Debes seleccionar algún pais en el panel de códigos.</p>
+    </div>
+    <div class="country-data">
+      <p v-if="selectedCountry.length > 0">
+      <table>
+        <tr>
+          <th>Nombre:</th>
+          <td>{{ currentCountryData.name }}</td>
+          <th>Población:</th>
+          <td>{{ currentCountryData.population * 1000000 }} millones</td>
+        </tr>
+        <tr>
+          <th>PIB:</th>
+          <td>{{ currentCountryData.pib }} millones</td>
+          <th>Área:</th>
+          <td>{{ currentCountryData.area }} km2</td>
+        </tr>
+        <tr>
+          <th>Renta:</th>
+          <td>{{ currentCountryData.income }}</td>
+          <th>PIB/Habitante:</th>
+          <td>{{ parseFloat(currentCountryData.pib / currentCountryData.population).toFixed(2) }}</td>
+        </tr>
+        <tr>
+          <th><button>Eliminar</button></th>
+
+        </tr>
+
+      </table>
+      </p>
+      <p v-else>No hay datos que mostrar</p>
+    </div>
+    <div class="options">
+      <p>
+        <label v-for="(option, index) in dataTypes" :key="index"><input type="radio" name="option"
+            @click="dataToChart">{{
+              option
+            }}</label>
+      </p>
+    </div>
+    <div class="chart">
+      <GoogleChart :data="selectedDataToChart" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.parent {
+  background-color: black;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: auto repeat(3, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  height: 100vh;
+  margin: -2rem;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.header {
+  grid-area: 1 / 1 / 2 / 6;
+  background-color: aquamarine;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.header h1 {
+  margin: 2px;
+}
+
+.div-codes {
+  grid-area: 2 / 1 / 6 / 2;
+  background-color: azure;
+  overflow-y: auto;
+}
+
+.name {
+  grid-area: 2 / 2 / 3 / 3;
+  background-color: antiquewhite;
+}
+
+.selected {
+  grid-area: 2 / 5 / 4 / 6;
+  background-color: lightyellow;
+  overflow-y: auto;
+}
+
+.country-data {
+  grid-area: 2 / 3 / 3 / 5;
+  background-color: lightseagreen;
+}
+
+.options {
+  grid-area: 3 / 2 / 4 / 5;
+  background-color: bisque;
+}
+
+.chart {
+  grid-area: 4 / 2 / 5 / 6;
+  background-color: aqua;
+}
+
+ul li {
+  text-align: left;
+  font-size: 0.9rem;
+}
+
+ul li:hover {
+  font-weight: bold;
+  cursor: pointer
+}
+
+ul li button {
+  font-size: 0.6rem;
+  margin: 2px;
+}
+
+h2 {
+  font-size: 1.2rem;
 }
 </style>
---
+++ src/components/InsertName.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const insertedname = ref('');
+</script>
+<template>
+  <p>Inserta tu nombre:
+    <input type="text" v-model="insertedname">
+  </p>
+  <p v-if="insertedname">Tu nombre <strong>{{ insertedname }}</strong> tiene <strong>{{ insertedname.length }}
+    </strong> letras
+  </p>
+</template>
+
+
+
+<style scoped></style>
